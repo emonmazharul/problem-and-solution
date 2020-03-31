23ef0f738d9f89ea49fd897072027af1946c63f8
Mazharul Emon <emonmazharul@yandex.com>
2020-03-31 21:38:00 +0600

    fibonacci sequence solved!

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -8,34 +8,34 @@ const {
 describe('should calcucalte fibonacci correctly', () => {
 
     it('should find correct fabonacci', () => {
-    	assert.equal(fibonacci(1), [1]);
+    	assert.deepEqual(fibonacci(1), [1]);
     })
 	it('should find correct fabonacci', () => {
-    	assert.equal(fibonacci(2), [1, 1]);
+    	assert.deepEqual(fibonacci(2), [1, 1]);
     })
 	it('should find correct fabonacci', () => {
-    	assert.equal(fibonacci(3), [1, 1, 2]);
+    	assert.deepEqual(fibonacci(3), [1, 1, 2]);
     })
 	it('should find correct fabonacci', () => {
-    	assert.equal(fibonacci(4), [1, 1, 2, 3]);
+    	assert.deepEqual(fibonacci(4), [1, 1, 2, 3]);
     })
 	it('should find correct fabonacci', () => {
-    	assert.equal(fibonacci(5),[1, 1, 2, 3, 5] );
+    	assert.deepEqual(fibonacci(5),[1, 1, 2, 3, 5] );
     })
 	it('should find correct fabonacci', () => {
-    	assert.equal(fibonacci(6), [1, 1, 2, 3, 5, 8]);
+    	assert.deepEqual(fibonacci(6), [1, 1, 2, 3, 5, 8]);
     })	
 	it('should find correct fabonacci', () => {
-    	assert.equal(fibonacci(7),[1, 1, 2, 3, 5, 8, 13] );
+    	assert.deepEqual(fibonacci(7),[1, 1, 2, 3, 5, 8, 13] );
     })
 	it('should find correct fabonacci', () => {
-    	assert.equal(fibonacci(8),[1, 1, 2, 3, 5, 8, 13, 21] );
+    	assert.deepEqual(fibonacci(8),[1, 1, 2, 3, 5, 8, 13, 21] );
     })
    	it('should find correct fabonacci', () => {
-    	assert.equal(fibonacci(9), [1, 1, 2, 3, 5, 8, 13, 21, 34]);
+    	assert.deepEqual(fibonacci(9), [1, 1, 2, 3, 5, 8, 13, 21, 34]);
     })
 	it('should find correct fabonacci', () => {
-    	assert.equal(fibonacci(10),[1, 1, 2, 3, 5, 8, 13, 21, 34, 55] );
+    	assert.deepEqual(fibonacci(10), [1, 1, 2, 3, 5, 8, 13, 21, 34, 55] );
     })
 })
 
@@ -44,13 +44,11 @@ describe('should calcucalte fibonacci correctly', () => {
 function fibonacci(n) {
     let res = [1];
     for (let i = 1; i < n; i++) {
-    	console.log(res[res.length-1], res[res.length-2])
     	res.push((res[res.length-1])+(res[res.length-2] || 0) )       
     }
 
     return res;
 }
-
 
 mocha.run()
 
